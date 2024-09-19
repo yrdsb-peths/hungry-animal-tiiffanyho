96a8d171eb7d08a4e9df64b0a22a171b6a9891a1
t1ffanyh <Tiffanyho0903@gmail.com>
2024-09-19 13:10:11 -0400

    game over

--- Bread.java
+++ Bread.java
@@ -14,6 +14,17 @@ public class Bread extends Actor
      */
     public void act()
     {
-        setLocation(getX(), getY() + 1);
+        //Apple falls downwards.
+        int x = getX();
+        int y = getY() + 2;
+        setLocation(x, y);
+        
+        //Remove apple and draw game over when apple gets to bottom
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -17,7 +17,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
         //Create the elephant object
         Elephant elephant = new Elephant();
@@ -30,6 +30,16 @@ public class MyWorld extends World
         createBread();
     }
     
+    /**
+     * End the game and draw "grameOver"
+     * 
+     */
+    
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
     /**
      * Increase score
      */
