762b1e2f67811aaebe1edca92896468d4d9db761
t1ffanyh <Tiffanyho0903@gmail.com>
2024-09-19 13:31:27 -0400

    sound

--- Elephant.java
+++ Elephant.java
@@ -12,16 +12,18 @@ public class Elephant extends Actor
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootSound eatingSound = new GreenfootSound("eating.mp3");
+    
     public void act()
     {
         // Add your action code here.
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-1);
+            move(-3);
         } 
         else if(Greenfoot.isKeyDown("right"))
         {
-            move(1);
+            move(3);
         }
         eat();
     }
@@ -38,6 +40,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createBread();
             world.increaseScore();
+            eatingSound.play();
         }
     }
 }
